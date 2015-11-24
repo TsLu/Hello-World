7b98f8b53a418ba645d8b233339b2f10bdc972dd
TsLu <601501823@qq.com>
2015-11-24 20:34:20 +0800

    Test for Git for GitHub

---
+++ Test-hello/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+/**
+ * Test for git used for IntelliJ IDEA
+ * Created by lutingshu on 2015/11/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("测试git托管到github的功能");
+        System.out.println("Hello World!");
+    }
+}
