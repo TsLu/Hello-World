c87f6f526566edec22ead9801519f05c72cc57a5
TsLu <601501823@qq.com>
2015-11-24 21:56:35 +0800

    Test for Git for GitHub

--- Test-hello/src/HelloWorld/HelloWorld.java
+++ Test-hello/src/HelloWorld/HelloWorld.java
@@ -6,6 +6,7 @@ package HelloWorld;
  */
 public class HelloWorld {
     public static void main(String[] args){
+
         System.out.println("测试git托管到github的功能");
         System.out.println("Hello World!");
     }
