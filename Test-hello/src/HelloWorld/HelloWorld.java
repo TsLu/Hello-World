2a102750797a74de60d71faa90131d98ff911127
TsLu <601501823@qq.com>
2015-11-25 18:33:50 +0800

    Test for Git for GitHub

--- Test-hello/src/HelloWorld/HelloWorld.java
+++ Test-hello/src/HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 package HelloWorld;
 
+import java.util.Scanner;
+
 /**
  * Test for git used for IntelliJ IDEA
  * Created by lutingshu on 2015/11/24.
@@ -7,7 +9,12 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args){
 
-        System.out.println("测试git托管到github的功能");
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入圆的半径");
+        double r = input.nextDouble();
+
+        double ear = Math.PI * r * r;
+        System.out.println("半径为："+r+"的圆的面积为："+ear);
+
     }
 }
